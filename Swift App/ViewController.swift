95039e4f974dbc8949f2268f06b5d9d28ea371fc
Emma <emmamtaumoepeau@gmail.com>
2020-04-23 18:04:27 +1200

    First commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    
+    @IBOutlet weak var MyLABEL: UILabel!
+    
+    @IBAction func ButtonTapped(_ sender: Any) {
+        
+        buttonCount+=1
+        if buttonCount>=3{
+            view.backgroundColor = UIColor.purple
+            MyLABEL.text = "Emma"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
 
