c72b508f0d45fb9f27d0af22078cc38d7d683f38
Emma <emmamtaumoepeau@gmail.com>
2020-04-23 17:06:25 +1200

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Emma on 23/04/20.
+//  Copyright © 2020 Emma Taumoepeau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
